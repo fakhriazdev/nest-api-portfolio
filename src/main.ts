7f0fcd4c6a8b5bb31313d764c47c6b0f5a819ae1
FakhriAz Dev <fakhriazdev@gmail.com>
2024-06-06 09:47:19 +0700

    [TEST DEPLOY]: CORS Enabled

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   await app.listen(3000);
-  console.log('Nest application is running!');
 }
-
 bootstrap();
-
-// Function to check connection
-async function checkConnection() {
-  try {
-    await NestFactory.create(AppModule);
-    // Perform any necessary checks here
-    console.log('Connection to Nest application successful!');
-  } catch (error) {
-    console.error('Error connecting to Nest application:', error);
-  }
-}
-
-// Call the function to check connection
-checkConnection();
