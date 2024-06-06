8053511cf162f38344654d9f17aaec7166d27b29
FakhriAz Dev <fakhriazdev@gmail.com>
2024-06-06 09:57:44 +0700

    [TEST DEPLOY]: PORT & Hostname

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
