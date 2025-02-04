3aa71a503397a8780e5fff14db201f2a91835333
fakhriazdev <fakhriazdev@gmail.com>
2025-02-04 17:58:46 +0700

    add port env

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     parseOptions: {}, // Optional cookie parsing options
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
